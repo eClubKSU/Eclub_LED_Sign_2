65407de6f1ffd355bd3f8853cacf907de7b801f3
ZwzollersKSU <zwzollers@ksu.edu>
2025-03-28 22:12:23 -0500

    working on bitmap generator

--- bitmap_gen/target/debug/bitmap_gen.d
+++ bitmap_gen/target/debug/bitmap_gen.d
@@ -1,1 +1,1 @@
-C:\Users\zwzol\Documents\Code\GIT\Eclub_LED_Sign_V2\bitmap_gen\target\debug\bitmap_gen.exe: C:\Users\zwzol\Documents\Code\GIT\Eclub_LED_Sign_V2\bitmap_gen\src\main.rs
+/home/zwzollers/Documents/GIT/Eclub_LED_Sign_V2/bitmap_gen/target/debug/bitmap_gen: /home/zwzollers/Documents/GIT/Eclub_LED_Sign_V2/bitmap_gen/src/main.rs
